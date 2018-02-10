74ca7ef5ae8a69cb533f32dea986a93c0e2add8a
jdan <luckyrass@163.com>
2018-02-10 16:47:35 +0800

    red package version 1.0

--- RedPackage.java
+++ RedPackage.java
@@ -11,7 +11,8 @@ public class RedPackage {
     private RedPackage() {
     }
 
-    public static double[] generateMoneyVector(double money, int peopleNum) {
+    // method 1
+    public static double[] generateMoneyAverageVector(double money, int peopleNum) {
         // money in red package is not enough
         if (money < peopleNum * 0.01) {
             return new double[0];
@@ -28,29 +29,72 @@ public class RedPackage {
         return redPackageArray;
     }
 
+    // method 2
+    public static double[] generateMoneyGaussianVector(double money, int peopleNum) {
+        // money in red package is not enough
+        if (money < peopleNum * 0.01) {
+            return new double[0];
+        }
+        double[] redPackageArray = new double[peopleNum];
+        Arrays.fill(redPackageArray, 0);
+        // generate a normal distribution array
+        double moneyLeft = money - peopleNum * 0.01;
+        double mean = moneyLeft / peopleNum;
+        double stddev = mean / 2;
+        Random rand = new Random(System.currentTimeMillis());
+        for (int i = 0; i < peopleNum - 1; i++) {
+            double shareMoney = rand.nextGaussian() * stddev + mean;
+            if (shareMoney < 0) {
+                shareMoney = 0;
+            }
+            if (shareMoney > moneyLeft) {
+                shareMoney = moneyLeft;
+            }
+            redPackageArray[i] = shareMoney + 0.01;
+            moneyLeft -= shareMoney;
+        }
+        redPackageArray[peopleNum-1] = moneyLeft + 0.01;
+        return redPackageArray;
+    }
+
     public static void main(String[] args) {
         // useCase1: 10, 5
         double price1 = 10;
         int peopleNum1 = 5;
+        System.out.println("==========generateMoneyAverageVector=========");
         System.out.printf("price1 = %.2f, peopleNum1 = %d\n", price1, peopleNum1);
-        for (double price : RedPackage.generateMoneyVector(price1, peopleNum1)) {
-            System.out.printf("%.2f,", price);
+        for (int i = 0; i < 3; i++) {
+            for (double price : RedPackage.generateMoneyAverageVector(price1, peopleNum1)) {
+                System.out.printf("%.2f,", price);
+            }
+            System.out.println();
         }
-        System.out.println();
         // useCase2: 5, 10
         double price2 = 5;
         int peopleNum2 = 10;
         System.out.printf("price2 = %.2f, peopleNum2 = %d\n", price2, peopleNum2);
-        for (double price : RedPackage.generateMoneyVector(price2, peopleNum2)) {
-            System.out.printf("%.2f,", price);
+        for (int i = 0; i < 3; i++) {
+            for (double price : RedPackage.generateMoneyAverageVector(price2, peopleNum2)) {
+                System.out.printf("%.2f,", price);
+            }
+            System.out.println();
         }
-        System.out.println();
-        // useCase3: 0.05, 10
-        double price3 = 0.05;
-        int peopleNum3 = 10;
-        System.out.printf("price3 = %.2f, peopleNum3 = %d\n", price3, peopleNum3);
-        for (double price : RedPackage.generateMoneyVector(price3, peopleNum3)) {
-            System.out.printf("%.2f,", price);
+
+        System.out.println("==========generateMoneyAverageVector=========");
+        System.out.printf("price1 = %.2f, peopleNum1 = %d\n", price1, peopleNum1);
+        for (int i = 0; i < 3; i++) {
+            for (double price : RedPackage.generateMoneyGaussianVector(price1, peopleNum1)) {
+                System.out.printf("%.2f,", price);
+            }
+            System.out.println();
+        }
+
+        System.out.printf("price2 = %.2f, peopleNum2 = %d\n", price2, peopleNum2);
+        for (int i = 0; i < 3; i++) {
+            for (double price : RedPackage.generateMoneyGaussianVector(price2, peopleNum2)) {
+                System.out.printf("%.2f,", price);
+            }
+            System.out.println();
         }
     }
 }
